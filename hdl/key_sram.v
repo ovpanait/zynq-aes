0171cffcd442b2a27e261e228f249af6c0498b5f
Ovidiu Panait <ovidiu.panait@windriver.com>
2019-01-15 18:01:29 +0200

    AES: key_sram.v: Add synchronous RAM module for storing round keys
    Signed-off-by: Ovidiu Panait <ovidiu.panait@gmail.com>

---
+++ hdl/key_sram.v
@@ -0,0 +1,26 @@
+module key_sram #
+  (
+   parameter integer ADDR_WIDTH = 4,
+   parameter integer DATA_WIDTH = 128, 
+   parameter integer DEPTH = 256
+   ) 
+   (
+    input 			clk,
+
+    input [0:DATA_WIDTH-1] 	i_data,
+    input [0:ADDR_WIDTH-1] 	addr,
+    input 			w_e,
+    input 			r_e,
+
+    output reg [0:DATA_WIDTH-1] o_data
+    );
+
+   reg [0:DATA_WIDTH-1] 	sram [0:DEPTH-1];
+
+   always @ (posedge clk) begin
+      if (w_e)
+        sram[addr] <= i_data;
+      else if (r_e)
+        o_data <= sram[addr];
+   end
+endmodule
