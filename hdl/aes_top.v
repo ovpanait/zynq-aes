1301cf0a1db04f6175d057e4036ad652d083143a
Ovidiu Panait <ovidiu.panait@windriver.com>
2019-02-06 19:25:49 +0200

    Revert "AES: aes_top.v: add axi4lite control/status register support"
    This reverts commit f18d4b832c525ea65e09274a78752024ce80f9a2.
    
    A better idea would be to get rid of control/status registers entirely
    and send 128/256 bits of data from the PS into an AXI stream fifo.

--- hdl/aes_top.v
+++ hdl/aes_top.v
@@ -1,113 +1,72 @@
 `include "aes.vh"
 
 module aes_top(
-        input                 clk,
-        input                 reset,
-        input                 en,
+	       input 		   clk,
+	       input 		   reset,
+	       input 		   en,
 
-        // AES signals
-        input [0:`KEY_S-1]      aes_key,
-        input [0:`BLK_S-1]      aes_plaintext,
-        output [0:`BLK_S-1]     aes_ciphertext,
+	       input [0:`KEY_S-1]  aes_key,
+	       input [0:`BLK_S-1]  aes_plaintext,
 
-        // Control/Status registers
-        input [`CTRL_S-1:0]     ctrl,
-        output [`STATUS_S-1:0]  status,
+	       output [0:`BLK_S-1] aes_ciphertext,
+	       output 		   en_o
+	       );
 
-        output reg              en_o
-);
+   wire [0:`KEY_S-1] 		  round_key_in;
+   wire [0:`KEY_S-1] 		  round_key_out;
+   wire [0:`Nk-1] 		  key_round_no;
+   wire [0:`Nk-1] 		  encrypt_round_no;
 
-wire [0:`KEY_S-1]       round_key_in;
-wire [0:`KEY_S-1]       round_key_out;
-wire [0:`Nk-1]          key_round_no;
-wire [0:`Nk-1]          encrypt_round_no;
+   // SRAM signls
+   wire 			  w_e;
+   wire 			  r_e;
+   wire [0:3] 			  addr;
 
-// SRAM signals
-wire                          w_e;
-wire                          r_e;
-wire [0:3]                    addr;
+   wire 			  en_o_round_key;
+   wire 			  en_o_cipher;
 
-// Enable signals
-wire                          en_cipher;
-wire                          en_round_key;
+   round_key round_key_gen(
+			   .clk(clk),
+			   .reset(reset),
+			   .en(en),
 
-wire                          en_o_round_key;
-wire                          en_o_cipher;
+			   .key(aes_key),
+
+			   .round_key(round_key_in),
+			   .round_no(key_round_no),
+			   .w_e(w_e),
+
+			   .en_o(en_o_round_key)
+			   );
 
 
-// SRAM holding round keys
-assign addr = w_e ? key_round_no : encrypt_round_no;
+   assign addr = w_e ? key_round_no : encrypt_round_no;
 
-key_sram sram(
-        .clk(clk),
+   key_sram sram(
+		 .clk(clk),
 
-        .addr(addr),
-        .i_data(round_key_in),
-        .w_e(w_e),
+		 .addr(addr),
+		 .i_data(round_key_in),
+		 .w_e(w_e),
 
-        .o_data(round_key_out),
-        .r_e(r_e)
-);
+		 .o_data(round_key_out),
+		 .r_e(r_e)
+		 );
 
-// Key expansion module
-round_key round_key_gen(
-        .clk(clk),
-        .reset(reset),
-        .en(en_round_key),
+   cipher encrypt_blk(
+		      .clk(clk),
+		      .reset(reset),
+		      .en(en_o_round_key),
 
-        .key(aes_key),
+		      .plaintext(aes_plaintext),
+		      .key(round_key_out),
+		      .round_no(encrypt_round_no),
+		      .r_e(r_e),
 
-        .round_key(round_key_in),
-        .round_no(key_round_no),
-        .w_e(w_e),
+		      .ciphertext(aes_ciphertext),
+		      .en_o(en_o_cipher)
+		      );
 
-        .en_o(en_o_round_key)
-);
-
-// Encryption module
-cipher encrypt_blk(
-        .clk(clk),
-        .reset(reset),
-        .en(en_cipher),
-
-        .plaintext(aes_plaintext),
-        .key(round_key_out),
-        .round_no(encrypt_round_no),
-        .r_e(r_e),
-
-        .ciphertext(aes_ciphertext),
-        .en_o(en_o_cipher)
-);
-
-// Control register logic
-assign en_cipher = (en == 1'b1) && (ctrl == `CTRL_ENCRYPT);
-assign en_round_key = (en == 1'b1) && (ctrl == `CTRL_KEY);
-
-// Status register logic
-reg [`STATUS_S-1:0] status_reg;
-
-always @(posedge clk) begin
-        if (reset == 1'b1) begin
-                status_reg <= {`STATUS_S{1'b0}};
-                en_o <= 1'b0;
-        end 
-        else begin
-                en_o <= 1'b0;
-
-                if (en == 1'b1)
-                        status_reg <= {`STATUS_S{1'b0}};
-
-                if (en_o_round_key == 1'b1) begin
-                        status_reg <= (status_reg | `S_KEY_MASK);
-                        en_o <= 1'b1;
-                end
-                if (en_o_cipher == 1'b1) begin
-                        status_reg <= (status_reg | `S_CIPHER_MASK);
-                        en_o <= 1'b1;
-                end
-        end
-end
-
-assign status = status_reg;
+   assign en_o = en_o_cipher;
 
 endmodule
--- tb/aes_top/tb_main.sv
+++ tb/aes_top/tb_main.sv
@@ -6,123 +6,93 @@
 
 module tb_main();
 
-// Module instantiation
-reg clk;
-reg reset;
-reg en;
+   // Module instantiation
+   reg clk;
+   reg reset;
+   reg en;
 
-reg [`CTRL_S-1:0]	ctrl_reg;
-reg [0:`KEY_S-1]	aes_key;
-reg [0:`BLK_S-1]	aes_plaintext;
+   reg [0:`KEY_S-1] aes_key;
+   reg [0:`BLK_S-1] aes_plaintext;
 
-wire [`STATUS_S-1:0]	status_reg;
-wire [0:`BLK_S-1]	aes_ciphertext;
-wire 	    		en_o;
+   wire [0:`BLK_S-1] aes_ciphertext;
+   wire 	     en_o;
 
-aes_top DUT (
-	.clk(clk),
-	.reset(reset),
-	.en(en),
+   aes_top DUT (
+		.clk(clk),
+		.reset(reset),
+		.en(en),
 
-	.ctrl(ctrl_reg),
-	.aes_key(aes_key),
-	.aes_plaintext(aes_plaintext),
+		.aes_key(aes_key),
+		.aes_plaintext(aes_plaintext),
 
-	.status(status_reg),
-	.aes_ciphertext(aes_ciphertext),
-	.en_o(en_o)
-);
+		.aes_ciphertext(aes_ciphertext),
+		.en_o(en_o)
+		);
 
-// Test functions
+   // Test functions
 `include "test_fc.vh"
 
-// Auxiliary counters
-integer 	     i;
+   // Auxiliary counters
+   integer 	     i;
 
-// Test results
-initial begin
-	clk <= 0;
-	forever #(`PERIOD) clk = ~clk;
-end
+   // Test results
+   initial begin
+      clk <= 0;
+      forever #(`PERIOD) clk = ~clk;
+   end
 
-initial begin
-	reset <= 0;
-	@(posedge clk); //may need several cycles for reset
-	@(negedge clk) reset = 1;
-end
+   initial begin
+      reset <= 0;
+      @(posedge clk); //may need several cycles for reset
+      @(negedge clk) reset = 1;
+   end
 
-initial begin
-	errors = 0; // reset error count
+   initial begin
+      errors = 0; // reset error count
 
-	// Testcase init
-	wait(reset)
+      // Testcase init
+      wait(reset)
 	@(posedge clk);
-	@(negedge clk) reset = 0;
+      @(negedge clk) reset = 0;
 
-	// Testcase
+      // Testcase
+      @(negedge clk) begin
+	 en = 1;
+	 aes_key =  {
+		 8'h54, 8'h68, 8'h61, 8'h74,
+		 8'h73, 8'h20, 8'h6D, 8'h79,
+		 8'h20, 8'h4B, 8'h75, 8'h6E,
+		 8'h67, 8'h20, 8'h46, 8'h75
+		 };
 
-	// Load control register with key expansion command
-	@(negedge clk) begin
-		ctrl_reg = `CTRL_KEY;
-	end
+	 aes_plaintext =  {
+		       8'h54, 8'h77, 8'h6F, 8'h20,
+		       8'h4F, 8'h6E, 8'h65, 8'h20,
+		       8'h4E, 8'h69, 8'h6E, 8'h65,
+		       8'h20, 8'h54, 8'h77, 8'h6F
+		       };
+      end
 
-	// Simulate DMA transfer completion of encryption key
-	@(negedge clk) begin
-		en = 1'b1;
-		aes_key =  {
-			8'h54, 8'h68, 8'h61, 8'h74,
-			8'h73, 8'h20, 8'h6D, 8'h79,
-			8'h20, 8'h4B, 8'h75, 8'h6E,
-			8'h67, 8'h20, 8'h46, 8'h75
-		};
+      @(negedge clk) begin
+	 en = 0;
+      end
 
-	end
+      @(posedge DUT.en_o);
 
-	@(negedge clk) begin
-		en = 1'b0;
-	end
+      // Test ciphertext output
+`define RES_AES_CIPHERTEXT `BLK_S'h29c3505f571420f6402299b31a02d73a
+      @(negedge clk);
+      tester #($size(aes_ciphertext))::verify_output(aes_ciphertext, `RES_AES_CIPHERTEXT);
 
-	// Wait for status register to indicate key expansion completion
-	wait (status_reg);
-	tester #($size(status_reg))::verify_output(status_reg, `S_KEY_MASK);
+      // Test 1 clock cycle
+      @(negedge clk);
+      tester #($size(en_o))::verify_output(en_o, `BLK_S'h0);
 
-	// Load control register with cipher command
-	@(negedge clk);
-	ctrl_reg = `CTRL_ENCRYPT;
+      // Testcase end
+      @(negedge clk) reset = 1;
+      @(negedge clk);
 
-	// Load plaintext
-	@(negedge clk) begin
-		en = 1'b1;
-
-		aes_plaintext =  {
-			8'h54, 8'h77, 8'h6F, 8'h20,
-			8'h4F, 8'h6E, 8'h65, 8'h20,
-			8'h4E, 8'h69, 8'h6E, 8'h65,
-			8'h20, 8'h54, 8'h77, 8'h6F
-		};
-	end
-
-	@(negedge clk) begin
-		en = 1'b0;
-	end
-
-	@(posedge en_o);
-
-	// Test ciphertext output
-	`define RES_AES_CIPHERTEXT `BLK_S'h29c3505f571420f6402299b31a02d73a
-	@(negedge clk);
-	tester #($size(status_reg))::verify_output(status_reg, `S_CIPHER_MASK);
-	tester #($size(aes_ciphertext))::verify_output(aes_ciphertext, `RES_AES_CIPHERTEXT);
-
-	// Test if enable disables after 1 clock cycle
-	@(negedge clk);
-	tester #($size(en_o))::verify_output(en_o, `BLK_S'h0);
-
-	// Testcase end
-	@(negedge clk) reset = 1;
-	@(negedge clk);
-
-	$display("\nSimulation completed with %d errors\n", errors);
-	$stop;
-end
+      $display("\nSimulation completed with %d errors\n", errors);
+      $stop;
+   end // initial begin
 endmodule
