168cec31b9b19a3e922d56938b9013cdcaf5823a
Ovidiu Panait <ovidiu.panait@windriver.com>
2019-02-06 19:22:47 +0200

    AES: aes_top.v: add axi4lite control/status register support
    Signed-off-by: Ovidiu Panait <ovidiu.panait@gmail.com>

--- hdl/aes.vh
+++ hdl/aes.vh
@@ -1,14 +1,24 @@
 `ifndef AES_H
- `define AES_H
+`define AES_H
 
- `define BYTE_S 8
- `define WORD_S (4 * `BYTE_S)
+`define BYTE_S 8
+`define WORD_S (4 * `BYTE_S)
 
- `define Nb 4
- `define Nk 4
- `define Nr 10
+`define Nb 4
+`define Nk 4
+`define Nr 10
 
- `define KEY_S 128
- `define BLK_S 128
+`define KEY_S 128
+`define BLK_S 128
 
+`define CTRL_S `WORD_S
+`define CTRL_ENCRYPT 32'hF3F3F3F3
+`define CTRL_KEY 32'hA1A1A1A1
+
+`define STATUS_S `WORD_S
+`define S_KEY_BIT 1
+`define S_CIPHER_BIT 2
+
+`define S_KEY_MASK (32'h1 << (`S_KEY_BIT - 1))
+`define S_CIPHER_MASK (32'h1 << (`S_CIPHER_BIT - 1))
 `endif
--- hdl/aes_top.v
+++ hdl/aes_top.v
@@ -1,72 +1,113 @@
 `include "aes.vh"
 
 module aes_top(
-	       input 		   clk,
-	       input 		   reset,
-	       input 		   en,
+        input                 clk,
+        input                 reset,
+        input                 en,
 
-	       input [0:`KEY_S-1]  aes_key,
-	       input [0:`BLK_S-1]  aes_plaintext,
+        // AES signals
+        input [0:`KEY_S-1]      aes_key,
+        input [0:`BLK_S-1]      aes_plaintext,
+        output [0:`BLK_S-1]     aes_ciphertext,
 
-	       output [0:`BLK_S-1] aes_ciphertext,
-	       output 		   en_o
-	       );
+        // Control/Status registers
+        input [`CTRL_S-1:0]     ctrl,
+        output [`STATUS_S-1:0]  status,
 
-   wire [0:`KEY_S-1] 		  round_key_in;
-   wire [0:`KEY_S-1] 		  round_key_out;
-   wire [0:`Nk-1] 		  key_round_no;
-   wire [0:`Nk-1] 		  encrypt_round_no;
+        output reg              en_o
+);
 
-   // SRAM signls
-   wire 			  w_e;
-   wire 			  r_e;
-   wire [0:3] 			  addr;
+wire [0:`KEY_S-1]       round_key_in;
+wire [0:`KEY_S-1]       round_key_out;
+wire [0:`Nk-1]          key_round_no;
+wire [0:`Nk-1]          encrypt_round_no;
 
-   wire 			  en_o_round_key;
-   wire 			  en_o_cipher;
+// SRAM signals
+wire                          w_e;
+wire                          r_e;
+wire [0:3]                    addr;
 
-   round_key round_key_gen(
-			   .clk(clk),
-			   .reset(reset),
-			   .en(en),
+// Enable signals
+wire                          en_cipher;
+wire                          en_round_key;
 
-			   .key(aes_key),
-
-			   .round_key(round_key_in),
-			   .round_no(key_round_no),
-			   .w_e(w_e),
-
-			   .en_o(en_o_round_key)
-			   );
+wire                          en_o_round_key;
+wire                          en_o_cipher;
 
 
-   assign addr = w_e ? key_round_no : encrypt_round_no;
+// SRAM holding round keys
+assign addr = w_e ? key_round_no : encrypt_round_no;
 
-   key_sram sram(
-		 .clk(clk),
+key_sram sram(
+        .clk(clk),
 
-		 .addr(addr),
-		 .i_data(round_key_in),
-		 .w_e(w_e),
+        .addr(addr),
+        .i_data(round_key_in),
+        .w_e(w_e),
 
-		 .o_data(round_key_out),
-		 .r_e(r_e)
-		 );
+        .o_data(round_key_out),
+        .r_e(r_e)
+);
 
-   cipher encrypt_blk(
-		      .clk(clk),
-		      .reset(reset),
-		      .en(en_o_round_key),
+// Key expansion module
+round_key round_key_gen(
+        .clk(clk),
+        .reset(reset),
+        .en(en_round_key),
 
-		      .plaintext(aes_plaintext),
-		      .key(round_key_out),
-		      .round_no(encrypt_round_no),
-		      .r_e(r_e),
+        .key(aes_key),
 
-		      .ciphertext(aes_ciphertext),
-		      .en_o(en_o_cipher)
-		      );
+        .round_key(round_key_in),
+        .round_no(key_round_no),
+        .w_e(w_e),
 
-   assign en_o = en_o_cipher;
+        .en_o(en_o_round_key)
+);
+
+// Encryption module
+cipher encrypt_blk(
+        .clk(clk),
+        .reset(reset),
+        .en(en_cipher),
+
+        .plaintext(aes_plaintext),
+        .key(round_key_out),
+        .round_no(encrypt_round_no),
+        .r_e(r_e),
+
+        .ciphertext(aes_ciphertext),
+        .en_o(en_o_cipher)
+);
+
+// Control register logic
+assign en_cipher = (en == 1'b1) && (ctrl == `CTRL_ENCRYPT);
+assign en_round_key = (en == 1'b1) && (ctrl == `CTRL_KEY);
+
+// Status register logic
+reg [`STATUS_S-1:0] status_reg;
+
+always @(posedge clk) begin
+        if (reset == 1'b1) begin
+                status_reg <= {`STATUS_S{1'b0}};
+                en_o <= 1'b0;
+        end 
+        else begin
+                en_o <= 1'b0;
+
+                if (en == 1'b1)
+                        status_reg <= {`STATUS_S{1'b0}};
+
+                if (en_o_round_key == 1'b1) begin
+                        status_reg <= (status_reg | `S_KEY_MASK);
+                        en_o <= 1'b1;
+                end
+                if (en_o_cipher == 1'b1) begin
+                        status_reg <= (status_reg | `S_CIPHER_MASK);
+                        en_o <= 1'b1;
+                end
+        end
+end
+
+assign status = status_reg;
 
 endmodule
