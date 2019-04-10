f8392591087fd71649a3b701d51129ed68736f9c
Ovidiu Panait <ovpanait@gmail.com>
2019-04-10 20:07:30 +0300

    AES: C: Move functions to common code
    Both encryption and decryption will make use of these functions.
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- C/aes.h
+++ C/aes.h
@@ -16,4 +16,36 @@ typedef uint8_t state_t[Nb][Nb];
 void key_expansion(uint8_t *sched, uint8_t *key);
 void cipher(uint8_t *plaintext, uint8_t *sched, uint8_t *cipher);
 
+#ifdef DEBUG
+static void dbg_state(const char *dbg_msg, state_t state)
+{
+	int row, col;
+
+	printf("%s:\n", dbg_msg);
+	for (row = 0; row < Nb; ++row) {
+		for (col = 0; col < Nb; ++col) {
+			printf("%02X ", state[row][col]);
+		}
+		printf("\n");
+	}
+	printf("\n");
+}
+#endif
+
+static void add_round_key(state_t state, uint8_t *key)
+{
+	int row, col;
+	int key_i;
+
+	for (col = 0, key_i = 0; col < Nb; ++col) {
+		for (row = 0; row < Nb; ++row, ++key_i) {
+			state[row][col] ^= key[key_i];
+		}
+	}
+
+	#ifdef DEBUG
+	dbg_state(__func__, state);
+	#endif
+}
+
 #endif
--- C/cipher.c
+++ C/cipher.c
@@ -3,22 +3,6 @@
 
 #include "aes.h"
 
-#ifdef DEBUG
-static void dbg_state(const char *dbg_msg, state_t state)
-{
-	int row, col;
-
-	printf("%s:\n", dbg_msg);
-	for (row = 0; row < Nb; ++row) {
-		for (col = 0; col < Nb; ++col) {
-			printf("%02X ", state[row][col]);
-		}
-		printf("\n");
-	}
-	printf("\n");
-}
-#endif
-
 // Populate state 2D array
 static void plaintext_to_state(state_t state, uint8_t *plaintext)
 {
@@ -46,22 +30,6 @@ static void state_to_ciphertext(state_t state, uint8_t *ciphertext)
 			ciphertext[ciphertext_i] = state[row][col];
 		}
 	}
-}
-
-static void add_round_key(state_t state, uint8_t *key)
-{
-	int row, col;
-	int key_i;
-
-	for (col = 0, key_i = 0; col < Nb; ++col) {
-		for (row = 0; row < Nb; ++row, ++key_i) {
-			state[row][col] ^= key[key_i];
-		}
-	}
-
-	#ifdef DEBUG
-	dbg_state(__func__, state);
-	#endif
 }
 
 // Substitute bytes
