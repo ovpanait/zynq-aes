ovpanait/zynq-aes

commit aac0d0259a2d77e49533819e2798fb99986ed4fd

hdl: Move aes_controller specific logic out of aes_top
Since aes_cmd processing and command codes should be specific to the
aes_controller module, we need to move them out of the aes_top module.

Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

hdl/aes_controller.v
@@ -51,7 +51,11 @@ reg [0:`BLK_S-1]  prev_aes_ecb_in_blk;
 reg [0:`WORD_S-1] __aes_cmd;
 reg [0: `BLK_S-1] aes_iv;
 reg [0:`KEY_S-1]  aes_key;
+
 reg               aes_start;
+wire              aes_cipher_mode;
+wire              aes_decipher_mode;
+wire              aes_key_exp_mode;
 
 wire [0:`BLK_S-1] aes_ecb_in_blk;
 wire [0:`BLK_S-1] aes_cbc_in_blk;
@@ -100,14 +104,38 @@ function is_CBC_dec(input [0:`WORD_S-1] cmd);
 	end
 endfunction
 
+function is_cipher_op(input [0:`WORD_S - 1] cmd);
+	is_cipher_op = (cmd == `CBC_ENCRYPT_128) || (cmd == `ECB_ENCRYPT_128);
+endfunction
+
+function is_decipher_op(input [0:`WORD_S - 1] cmd);
+	is_decipher_op = (cmd == `CBC_DECRYPT_128) || (cmd == `ECB_DECRYPT_128);
+endfunction
+
+function is_key_exp_op(input [0:`WORD_S - 1] cmd);
+	is_key_exp_op = (cmd == `SET_KEY_128);
+endfunction
+
+assign aes_cipher_mode = is_cipher_op(__aes_cmd);
+assign aes_start_cipher = aes_start && aes_cipher_mode;
+
+assign aes_decipher_mode = is_decipher_op(__aes_cmd);
+assign aes_start_decipher = aes_start && aes_decipher_mode;
+
+assign aes_key_exp_mode = is_key_exp_op(__aes_cmd);
+assign aes_start_key_exp = aes_start && aes_key_exp_mode;
+
 aes_top aes_mod(
 	.clk(clk),
 	.reset(reset),
+
 	.en(aes_start),
+	.cipher_mode(aes_cipher_mode),
+	.decipher_mode(aes_decipher_mode),
+	.key_exp_mode(aes_key_exp_mode),
 
 	.aes_op_in_progress(aes_op_in_progress),
 
-	.aes_cmd(__aes_cmd),
 	.aes_key(aes_key),
 	.aes_in_blk(aes_in_blk_reg),
 

hdl/aes_top.v
@@ -3,9 +3,12 @@
 module aes_top(
         input                   clk,
         input                   reset,
-        input                   en,
+	input                   en,
 
-        input [0:`WORD_S-1]     aes_cmd,
+	input                   cipher_mode,
+	input                   decipher_mode,
+	input                   key_exp_mode,
+
         input [0:`KEY_S-1]      aes_key,
         input [0:`BLK_S-1]      aes_in_blk,
 
@@ -21,14 +24,13 @@ wire [0:`Nk-1]       key_round_no;
 wire [0:`Nk-1]       encrypt_round_no;
 wire [0:`Nk-1]       decrypt_round_no;
 
-wire            en_i_round_key;
-wire            en_o_round_key;
+wire en_cipher;
+wire en_decipher;
+wire en_key_exp;
 
-wire            en_i_cipher;
-wire            en_o_cipher;
-
-wire            en_i_decipher;
-wire            en_o_decipher;
+wire en_o_round_key;
+wire en_o_cipher;
+wire en_o_decipher;
 
 wire [0:`BLK_S-1] __aes_out_blk_encrypt;
 wire [0:`BLK_S-1] __aes_out_blk_decrypt;
@@ -37,25 +39,14 @@ wire [0:`BLK_S-1] __aes_out_blk_decrypt;
 wire            w_e;
 wire [0:3]      addr;
 
-// Key expansion
-assign en_i_round_key = (en && aes_cmd == `SET_KEY_128);
+assign en_cipher = en && cipher_mode;
+assign en_decipher = en && decipher_mode;
+assign en_key_exp = en && key_exp_mode;
 
-round_key round_key_gen(
-        .clk(clk),
-        .reset(reset),
-        .en(en_i_round_key),
-
-        .key(aes_key),
-
-        .round_key(round_key_in),
-        .round_no(key_round_no),
-        .w_e(w_e),
-
-        .en_o(en_o_round_key)
-);
-
-// round keys SRAM block
-assign addr = w_e ? key_round_no : (encrypt_op ? encrypt_round_no : decrypt_round_no);
+assign addr =
+	key_exp_mode ? key_round_no :
+	cipher_mode ? encrypt_round_no :
+	decipher_mode ? decrypt_round_no : {4{1'b0}};
 
 block_ram #(
         .ADDR_WIDTH(4),
@@ -70,15 +61,24 @@ block_ram #(
         .o_data(round_key_out)
 );
 
-// Encryption / Decryption blocks
-assign encrypt_op = (aes_cmd == `ECB_ENCRYPT_128 || aes_cmd == `CBC_ENCRYPT_128);
-assign en_i_cipher = (en && encrypt_op);
-assign en_i_decipher = (en && (aes_cmd == `ECB_DECRYPT_128 || aes_cmd == `CBC_DECRYPT_128));
+round_key round_key_gen(
+        .clk(clk),
+        .reset(reset),
+        .en(en_key_exp),
+
+        .key(aes_key),
+
+        .round_key(round_key_in),
+        .round_no(key_round_no),
+        .w_e(w_e),
+
+        .en_o(en_o_round_key)
+);
 
 cipher encrypt_blk(
         .clk(clk),
         .reset(reset),
-        .en(en_i_cipher),
+        .en(en_cipher),
 
         .plaintext(aes_in_blk),
         .key(round_key_out),
@@ -91,7 +91,7 @@ cipher encrypt_blk(
 decipher decrypt_blk(
         .clk(clk),
         .reset(reset),
-        .en(en_i_decipher),
+        .en(en_decipher),
 
         .ciphertext(aes_in_blk),
         .round_key(round_key_out),
@@ -101,8 +101,10 @@ decipher decrypt_blk(
         .en_o(en_o_decipher)
 );
 
-assign aes_out_blk = en_o_cipher ? __aes_out_blk_encrypt :
-                (en_o_decipher ? __aes_out_blk_decrypt : {`BLK_S{1'b0}});
+assign aes_out_blk =
+	cipher_mode ? __aes_out_blk_encrypt :
+	decipher_mode ? __aes_out_blk_decrypt : {`BLK_S{1'b0}};
+
 assign en_o = en_o_cipher | en_o_decipher | en_o_round_key;
 
 always @(posedge clk) begin

commit 41b40b7b75e49f32aff5e9470fadcab8b2464dfb

hdl: aes_controller: Fix key expansion flag
Currently, the flag stays active for only one clock cycle because __aes_cmd is
prematurely reset to its original value. Reset __aes cmd just after key
expansion has finished.

Fixes: aac0d02 [hdl: Move aes_controller specific logic out of aes_top]
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

hdl/aes_controller.v
@@ -143,7 +143,7 @@ aes_top aes_mod(
 	.en_o(aes_done)
 );
 
-assign aes_in_blk = is_CBC_enc(__aes_cmd) ?  aes_cbc_in_blk : aes_ecb_in_blk;
+assign aes_in_blk = is_CBC_enc(aes_cmd) ?  aes_cbc_in_blk : aes_ecb_in_blk;
 assign aes_ecb_in_blk = swap_bytes128(in_fifo_data);
 assign aes_cbc_in_blk = aes_iv ^ aes_ecb_in_blk;
 
@@ -200,12 +200,12 @@ always @(posedge clk) begin
 			AES_START:
 			begin
 				state <= AES_START;
-				__aes_cmd <= aes_cmd;
 
 				if (!aes_op_in_progress)
 					in_fifo_read_tready <= 1'b1;
 
 				if (in_fifo_read_req) begin
+					__aes_cmd <= aes_cmd;
 					in_fifo_read_tready <= 1'b0;
 					aes_in_blk_reg <= aes_in_blk;
 					state <= AES_WAIT;
@@ -226,11 +226,11 @@ always @(posedge clk) begin
 					out_fifo_data <= swap_bytes128(aes_out_blk);
 					state <= AES_STORE_BLOCK;
 
-					if(is_CBC_enc(__aes_cmd)) begin
+					if(is_CBC_enc(aes_cmd)) begin
 						aes_iv <= aes_out_blk;
 					end
 
-					if(is_CBC_dec(__aes_cmd)) begin
+					if(is_CBC_dec(aes_cmd)) begin
 						out_fifo_data <= swap_bytes128(aes_iv ^ aes_out_blk);
 						aes_iv <= aes_in_blk_reg;
 					end