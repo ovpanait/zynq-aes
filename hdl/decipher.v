5561c1af1662c975c18fdbc6f2717c34d14cef57
Ovidiu Panait <ovpanait@gmail.com>
2019-10-13 12:43:19 +0300

    hdl: decipher: Simplify design and remove fsm
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- hdl/decipher.v
+++ hdl/decipher.v
@@ -8,88 +8,82 @@ module decipher (
         input [0:`BLK_S-1] ciphertext,
         input [0:`KEY_S-1] round_key,
 
-        output [0:`Nk-1] round_no,
         output reg [0:`BLK_S-1] plaintext,
+        output [0:`Nk-1] round_no,
         output reg en_o
 );
 
-localparam IDLE = 2'b0,
-        INIT_SRAM =2'b1,
-        FIRST_DECRYPT_ROUND = 2'b10,
-        DECRYPT_ROUND = 2'b11;
-
-reg [0:`Nk-1] _round_no;
-assign round_no = _round_no - 1'b1;
-
-reg [0:1] fsm_state;
+reg [`Nk:0] _round_no;
 
 reg [0:`BLK_S-1] decrypt_inv_shift_rows;
 reg [0:`BLK_S-1] decrypt_inv_sub_bytes;
 reg [0:`BLK_S-1] decrypt_add_round_key;
 reg [0:`BLK_S-1] decrypt_inv_mix_columns;
-reg [0:`BLK_S-1] decrypt_round_state;
+
+wire decipher_first_round;
+wire decipher_last_round;
+
+reg decipher_round_en;
+reg round_key_r_e;
 
 `include "aes_functions.vh"
+
+assign round_no = _round_no[`Nk-1:0];
+assign decipher_first_round = (_round_no == `Nr - 1'b1);
+assign decipher_last_round = (_round_no[`Nk] == 1'b1);
 
 always @(*) begin
           decrypt_inv_shift_rows = inv_shift_rows(plaintext);
           decrypt_inv_sub_bytes = inv_sub_bytes(decrypt_inv_shift_rows);
           decrypt_add_round_key = decrypt_inv_sub_bytes ^ round_key;
-          decrypt_inv_mix_columns = (_round_no == 1'b0) ? decrypt_add_round_key : inv_mix_cols(decrypt_add_round_key);
-
-          decrypt_round_state = decrypt_inv_mix_columns;
+          decrypt_inv_mix_columns = inv_mix_cols(decrypt_add_round_key);
 end
 
 always @(posedge clk) begin
-        if (reset == 1'b1) begin
-                _round_no <= 1'b0;
-                plaintext <= 1'b0;
-                en_o <= 1'b0;
-        end else begin
-                en_o <= 1'b0;
+	if (reset) begin
+		decipher_round_en <= 1'b0;
+		round_key_r_e <= 1'b0;
+		_round_no <= {`Nk+1{1'b1}};
+	end else begin
+		decipher_round_en <= round_key_r_e;
+		round_key_r_e <= 1'b0;
 
-                case (fsm_state)
-                IDLE:
-                begin
-                        if (en == 1'b1) begin
-                                plaintext <= ciphertext;
-                                _round_no <= `Nr + 1;
+		if (en) begin
+			round_key_r_e <= 1'b1;
+			_round_no <= `Nr;
+		end
 
-                                fsm_state <= INIT_SRAM;
-                        end
-                end
-                INIT_SRAM:
-                // Wait one clock cycle for the block RAM data to be available
-                begin
-                        fsm_state <= FIRST_DECRYPT_ROUND;
-                        
-                        _round_no <= round_no;
-                end
-                FIRST_DECRYPT_ROUND:
-                begin
-                        _round_no <= round_no;
+		if (!decipher_last_round) begin
+			_round_no <= round_no - 1'b1;
+			round_key_r_e <= 1'b1;
+		end
+	end
+end
 
-                        plaintext <= plaintext ^ round_key;
+always @(posedge clk) begin
+	if (reset) begin
+		plaintext <= {`BLK_S{1'b0}};
+	end else begin
+		if (decipher_round_en) begin
+			if (decipher_first_round)
+				plaintext <= ciphertext ^ round_key;
+			else if (decipher_last_round)
+				plaintext <= decrypt_add_round_key;
+			else
+				plaintext <= decrypt_inv_mix_columns;
+		end
+	end
+end
 
-                        fsm_state <= DECRYPT_ROUND;
-                end
-                DECRYPT_ROUND:
-                begin
-                        _round_no <= round_no;
+always @(posedge clk) begin
+	if (reset) begin
+		en_o <= 1'b0;
+	end else begin
+		en_o <= 1'b0;
 
-                        plaintext <= decrypt_round_state;
-
-                        if (_round_no == 1'b0) begin
-                                fsm_state <= IDLE;
-                                en_o <= 1'b1;
-                        end
-                end
-                default:
-                begin
-                        fsm_state <= IDLE;
-                end
-                endcase
-        end
+		if (decipher_round_en && decipher_last_round)
+			en_o <= 1'b1;
+	end
 end
 
 endmodule
