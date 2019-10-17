ovpanait/zynq-aes

commit 00d9aee49f7a6aadd5e722ffa1efcae364f5e8cd

hdl: aes_axi_stream_master: Adapt AXI logic for tvalid/tready handshake
* Get rid of the FSM and introduce two always separate always blocks
* Communicate with the FIFO via the tready/tvalid handshake and adapt the AXI
  stream logic to these changes

Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

hdl/aes_axi_stream_master.v
@@ -35,13 +35,13 @@ module aes_axi_stream_master #
 
 	input                                     processing_done,
 
-	input                                     aes_controller_out_fifo_w_e,
 	input  [FIFO_DATA_WIDTH-1:0]              aes_controller_out_fifo_data,
 
 	output                                    out_fifo_almost_full,
 	output                                    out_fifo_empty,
 	output                                    out_fifo_full,
-	output                                    out_fifo_ready,
+	output                                    out_fifo_write_tready,
+	input                                     out_fifo_write_tvalid,
 
 	output reg                                axis_master_done
 );
@@ -49,23 +49,23 @@ module aes_axi_stream_master #
 // FIFO signals
 
 wire fifo_almost_full;
-wire fifo_write_e;
-wire fifo_read_e;
+wire fifo_write_tready;
+wire fifo_write_tvalid;
+wire fifo_read_tready;
+wire fifo_read_tvalid;
 wire fifo_empty;
-wire fifo_ready;
 wire fifo_full;
 
 wire [FIFO_DATA_WIDTH-1:0] fifo_wdata;
 wire [FIFO_DATA_WIDTH-1:0] fifo_rdata;
 
-wire out_fifo_can_read;
-reg out_fifo_r_e;
+wire out_fifo_read_req;
 
 // AXI signals
 
 wire [FIFO_DATA_WIDTH-1:0] axis_blk_shift;
-reg [FIFO_ADDR_WIDTH-1:0]  axis_word_cnt;
-wire [FIFO_DATA_WIDTH-1:0] axis_blk;
+reg  [FIFO_ADDR_WIDTH-1:0] axis_word_cnt;
+reg  [FIFO_DATA_WIDTH-1:0] axis_blk;
 wire axis_transaction;
 wire axis_last_blk;
 wire axis_last_word;
@@ -82,37 +82,31 @@ fifo #(
 	.clk(m00_axis_aclk),
 	.reset(!m00_axis_aresetn),
 
-	.fifo_write_e(fifo_write_e),
+	.fifo_write_tvalid(fifo_write_tvalid),
+	.fifo_write_tready(fifo_write_tready),
 	.fifo_wdata(fifo_wdata),
 
-	.fifo_read_e(fifo_read_e),
+	.fifo_read_tvalid(fifo_read_tvalid),
+	.fifo_read_tready(fifo_read_tready),
 	.fifo_rdata(fifo_rdata),
 
 	.fifo_almost_full(fifo_almost_full),
 	.fifo_full(fifo_full),
-	.fifo_empty(fifo_empty),
-	.fifo_ready(fifo_ready)
+	.fifo_empty(fifo_empty)
 );
 
-assign fifo_write_e = aes_controller_out_fifo_w_e;
+assign fifo_read_tready = fifo_read_tvalid;
+assign fifo_write_tvalid = out_fifo_write_tvalid;
 assign fifo_wdata = aes_controller_out_fifo_data;
 
+assign out_fifo_write_tready = out_fifo_write_tvalid && !fifo_full;
 assign out_fifo_almost_full = fifo_almost_full;
 assign out_fifo_full = fifo_full;
 assign out_fifo_empty = fifo_empty;
-assign out_fifo_ready = fifo_ready;
 
-assign fifo_read_e = out_fifo_r_e;
-assign axis_blk = fifo_rdata;
+assign out_fifo_read_req = fifo_read_tready && fifo_read_tvalid;
 
 // AXI logic
-
-localparam AXIS_MASTER_IDLE = 2'b00;
-localparam AXIS_MASTER_INIT_SRAM = 2'b01;
-localparam AXIS_MASTER_INIT_TRANSFER = 2'b10;
-localparam AXIS_MASTER_TRANSFER = 2'b11;
-
-reg [0:1] axis_fsm_state;
 
 assign m00_axis_tdata  = axis_blk_shift[FIFO_DATA_WIDTH-`WORD_S +: `WORD_S];
 assign m00_axis_tstrb  = {(C_M_AXIS_TDATA_WIDTH/8){1'b1}};
@@ -125,56 +119,42 @@ assign axis_last_blk = processing_done && out_fifo_empty;
 assign axis_transaction = m00_axis_tready && axis_tvalid;
 assign axis_tlast =  axis_last_word;
 
-assign out_fifo_can_read = !out_fifo_r_e && fifo_ready && !fifo_empty;
-
 always @(posedge m00_axis_aclk) begin
 	if(!m00_axis_aresetn) begin
-		axis_fsm_state <= AXIS_MASTER_IDLE;
-		axis_master_done <= 1'b0;
-		out_fifo_r_e <= 1'b0;
-		axis_word_cnt <= 1'b0;
+		axis_blk <= 1'b0;
 		axis_tvalid <= 1'b0;
 	end 
 	else begin
-		out_fifo_r_e <= 1'b0;
-		axis_tvalid <= 1'b0;
-
-		case (axis_fsm_state)
-		AXIS_MASTER_IDLE:
-		begin
-			axis_fsm_state <= AXIS_MASTER_IDLE;
-			axis_master_done <= 1'b0;
-
-			if (out_fifo_can_read) begin
-				axis_fsm_state <= AXIS_MASTER_INIT_SRAM;
-				out_fifo_r_e <= 1'b1;
-			end
-		end
-		AXIS_MASTER_INIT_SRAM:
-		begin
-			axis_fsm_state <= AXIS_MASTER_TRANSFER;
-		end
-		AXIS_MASTER_TRANSFER:
-		begin
-			axis_fsm_state <= AXIS_MASTER_TRANSFER;
+		if (out_fifo_read_req) begin
+			axis_blk <= fifo_rdata;
 			axis_tvalid <= 1'b1;
-
-			if (axis_transaction) begin
-				axis_word_cnt <= axis_word_cnt + 1'b1;
-
-				if (axis_word_cnt == `Nb - 1'b1) begin
-					axis_fsm_state <= AXIS_MASTER_IDLE;
-					axis_word_cnt <= 1'b0;
-					axis_tvalid <= 1'b0;
-				end
-
-				if (axis_last_word) begin
-					axis_master_done <= 1'b1;
-					axis_tvalid <= 1'b0;
-				end
-			end
 		end
-		endcase
+
+		if (axis_transaction && axis_word_cnt == `Nb - 1'b1) begin
+			axis_tvalid <= 1'b0;
+		end
 	end
 end
+
+always @(posedge m00_axis_aclk) begin
+	if(!m00_axis_aresetn) begin
+		axis_master_done <= 1'b0;
+		axis_word_cnt <= 1'b0;
+	end else begin
+		axis_master_done <= 1'b0;
+
+		if (axis_transaction) begin
+			axis_word_cnt <= axis_word_cnt + 1'b1;
+
+			if (axis_word_cnt == `Nb - 1'b1) begin
+				axis_word_cnt <= 1'b0;
+			end
+
+			if (axis_last_word) begin
+				axis_master_done <= 1'b1;
+			end
+		end
+	end
+end
+
 endmodule

commit 916fd72f11fb2fbba9089c99a38f80510f3e73c9

hdl: aes_axi_stream_master: Fix AXI4STREAM_ERRM_TDATA_STABLE error
Currently, we might retrieve a block from the FIFO even though there is one
awaiting processing, causing the TDATA line to change while TVALID is stable.

Fixes: 00d9aee49f7a hdl: aes_axi_stream_master: Adapt AXI logic for tvalid/tready handshake
Signed-off-by: Ovidiu Panait <ovidiu.panait@windriver.com>

hdl/aes_axi_stream_master.v
@@ -95,7 +95,7 @@ fifo #(
 	.fifo_empty(fifo_empty)
 );
 
-assign fifo_read_tready = fifo_read_tvalid;
+assign fifo_read_tready = fifo_read_tvalid && !axis_tvalid;
 assign fifo_write_tvalid = out_fifo_write_tvalid;
 assign fifo_wdata = aes_controller_out_fifo_data;
 