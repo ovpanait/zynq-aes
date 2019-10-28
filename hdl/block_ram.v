ee546ad597a0b9f2db5b1d340c317030bd1840fd
Ovidiu Panait <ovidiu.panait@windriver.com>
2019-10-28 14:57:17 +0200

    hdl: block_ram: Reverse bit direction
    * Switch bit direction to MSB<-->LSB
    * Fix identation
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- hdl/block_ram.v
+++ hdl/block_ram.v
@@ -1,25 +1,23 @@
-module block_ram #
-  (
-   parameter integer ADDR_WIDTH = 9,
-   parameter integer DATA_WIDTH = 128, 
-   parameter integer DEPTH = 512          // 8192 bytes
-   ) 
-   (
-    input 			clk,
+module block_ram #(
+	parameter integer ADDR_WIDTH = 9,
+	parameter integer DATA_WIDTH = 128,
+	parameter integer DEPTH = 512          // 8192 bytes
+)(
+	input                       clk,
 
-    input [0:DATA_WIDTH-1] 	i_data,
-    input [0:ADDR_WIDTH-1] 	addr,
-    input 			w_e,
+	input [DATA_WIDTH-1:0]      i_data,
+	input [ADDR_WIDTH-1:0]      addr,
+	input                       w_e,
 
-    output reg [0:DATA_WIDTH-1] o_data
-    );
+	output reg [DATA_WIDTH-1:0] o_data
+);
 
-	reg [0:DATA_WIDTH-1] sram [0:DEPTH-1];
+reg [DATA_WIDTH-1:0] sram [0:DEPTH-1];
 
-	always @ (posedge clk) begin
+always @ (posedge clk) begin
 	o_data <= sram[addr];
 
 	if (w_e)
 		sram[addr] <= i_data;
-	end
+end
 endmodule
