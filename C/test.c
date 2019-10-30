7a6de30e0bc684ce38af0010a52ba1617447de22
Ovidiu Panait <ovpanait@gmail.com>
2019-10-30 13:58:37 +0200

    C: test: Prepare testcase for 256-bit keys
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- C/test.c
+++ C/test.c
@@ -17,17 +17,10 @@ static void dump_buffer(uint8_t *buf, unsigned int len, char *dbg)
 	printf("\n\n");
 }
 
-int main(int argc, char **argv)
+void test_crypto(void)
 {
 	uint8_t ciphertext[AES_BLOCK_SIZE];
 	uint8_t sched[(Nb * (Nr + 1)) * Nb];
-
-	uint8_t key[AES_KEYSIZE] = {
-		0x54, 0x68, 0x61, 0x74,
-		0x73, 0x20, 0x6D, 0x79,
-		0x20, 0x4B, 0x75, 0x6E,
-		0x67, 0x20, 0x46, 0x75
-	};
 
 	uint8_t plaintext[AES_BLOCK_SIZE] = {
 		0x54, 0x77, 0x6F, 0x20,
@@ -35,6 +28,17 @@ int main(int argc, char **argv)
 		0x4E, 0x69, 0x6E, 0x65,
 		0x20, 0x54, 0x77, 0x6F
 	};
+
+#ifdef AES_256
+	uint8_t key[AES_KEYSIZE];
+#else
+	uint8_t key[AES_KEYSIZE] = {
+		0x54, 0x68, 0x61, 0x74,
+		0x73, 0x20, 0x6D, 0x79,
+		0x20, 0x4B, 0x75, 0x6E,
+		0x67, 0x20, 0x46, 0x75
+	};
+#endif
 
 	dump_buffer(key, AES_KEYSIZE, "key:");
 	key_expansion(sched, key);
@@ -49,6 +53,11 @@ int main(int argc, char **argv)
 	memset(plaintext, 0x0, AES_BLOCK_SIZE);
 	decipher(ciphertext, plaintext, sched);
 	dump_buffer(plaintext, AES_BLOCK_SIZE, "plaintext:");
+}
+
+int main(int argc, char **argv)
+{
+	test_crypto();
 
 	return 0;
 }
