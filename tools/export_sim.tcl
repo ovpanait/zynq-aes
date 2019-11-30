ovpanait/zynq-aes

commit 08bb3f11870ab0b2c66acc9ea005c1255f4db0b1

tb : Add VERBOSE makefile parameter
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

tools/export_sim.tcl
@@ -11,6 +11,7 @@ OUTPUT_DIR        output directory for the generated project/simulation env
 exit 1
 }
 
+set verbose $env(VERBOSE)
 set hdl_include $env(HDL_INCLUDE)
 set tb_include $env(TB_INCLUDE)
 
@@ -18,6 +19,10 @@ set tb_dir [lindex $argv 0]
 set out_dir [lindex $argv 1]
 
 file mkdir ${out_dir}
+
+if {$verbose == 1} {
+	set verbose_define "SIMULATION_VERBOSE=1"
+}
 
 read_verilog [glob ./hdl/*.v]
 read_verilog [glob -nocomplain ./hdl/*.sv] -quiet
@@ -29,11 +34,12 @@ add_files -fileset [get_filesets sim_1] [glob -nocomplain ${tb_dir} ] -quiet
 
 set_property top tb_main [current_fileset -simset]
 
+set defines_list [list {SIMULATION=1} ${verbose_define}]
+
 export_simulation \
     -force \
     -simulator xsim \
     -directory ${out_dir}/export_sim \
     -include [list ${tb_dir}/include ${hdl_include} ${tb_include}] \
-    -define [list {SIMULATION=1}]
-
+    -define ${defines_list}
 exit

Makefile
@@ -1,3 +1,4 @@
+VERBOSE = 0
 VIP_SIM = 0
 
 HDL_DIR = $(shell readlink -f hdl)
@@ -42,14 +43,16 @@ $(IP_REPO_DIR): $(HDL_SOURCES)
 
 $(SIM_PROJ): $(IP_REPO_DIR)
 	rm -rf $@
-	HDL_INCLUDE=$(HDL_INCLUDE) TB_INCLUDE=$(TB_INCLUDE) vivado -mode tcl \
+	HDL_INCLUDE=$(HDL_INCLUDE) TB_INCLUDE=$(TB_INCLUDE) \
+	vivado -mode tcl \
 	-source "$(TOOLS_DIR)/create_axi_stream_sim_proj.tcl" \
 	-nolog -nojour \
 	-tclargs $(IP_REPO_DIR) $(TB_DIR) $(SIM_DIR)
 
 $(XSIM_DIR): $(HDL_SOURCES)
 	rm -rf $@
-	HDL_INCLUDE=$(HDL_INCLUDE) TB_INCLUDE=$(TB_INCLUDE) vivado -mode tcl \
+	HDL_INCLUDE=$(HDL_INCLUDE) TB_INCLUDE=$(TB_INCLUDE) VERBOSE=$(VERBOSE) \
+	vivado -mode tcl \
 	-source "$(TOOLS_DIR)/export_sim.tcl" \
 	-nolog -nojour \
 	-tclargs $(TB_DIR) $(SIM_DIR)

commit 5b0ba0a79143fcc8e18c34d021c36b39fa0c577d

tools: export_sim: Add default value for verbose_define
Fixes: 08bb3f1 [tb : Add VERBOSE makefile parameter]
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

tools/export_sim.tcl
@@ -11,6 +11,8 @@ OUTPUT_DIR        output directory for the generated project/simulation env
 exit 1
 }
 
+set verbose_define {}
+
 set verbose $env(VERBOSE)
 set hdl_include $env(HDL_INCLUDE)
 set tb_include $env(TB_INCLUDE)