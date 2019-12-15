ovpanait/zynq-aes

commit ea0fc4c00d8f069b151aff640d138c25ff4ee2ad

hdl: Get rid of aes controller master --> slave feedback loop
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

hdl/aes_axi_stream_master.v
@@ -41,9 +41,7 @@ module aes_axi_stream_master #
 	output                                    out_fifo_empty,
 	output                                    out_fifo_full,
 	output                                    out_fifo_write_tready,
-	input                                     out_fifo_write_tvalid,
-
-	output reg                                axis_master_done
+	input                                     out_fifo_write_tvalid
 );
 
 // FIFO signals
@@ -138,20 +136,13 @@ end
 
 always @(posedge m00_axis_aclk) begin
 	if(!m00_axis_aresetn) begin
-		axis_master_done <= 1'b0;
 		axis_word_cnt <= 1'b0;
 	end else begin
-		axis_master_done <= 1'b0;
-
 		if (axis_transaction) begin
 			axis_word_cnt <= axis_word_cnt + 1'b1;
 
 			if (axis_word_cnt == `Nb - 1'b1) begin
 				axis_word_cnt <= 1'b0;
-			end
-
-			if (axis_last_word) begin
-				axis_master_done <= 1'b1;
 			end
 		end
 	end

hdl/aes_axi_stream_slave.v
@@ -31,7 +31,6 @@ module aes_axi_stream_slave #(
 	input [C_S_AXIS_TDATA_WIDTH-1 : 0]      s00_axis_tdata,
 	input [(C_S_AXIS_TDATA_WIDTH/8)-1 : 0]  s00_axis_tstrb,
 
-	input                                   axis_master_done,
 	input                                   aes_controller_in_fifo_r_e,
 
 	output reg [`WORD_S-1:0]                axis_cmd,
@@ -195,7 +194,7 @@ always @(posedge s00_axis_aclk) begin
 			axis_packet_end <= 1'b1;
 		end
 
-		if (axis_master_done) begin
+		if (axis_slave_done && in_fifo_empty) begin
 			axis_packet_end <= 1'b0;
 		end
 	end
@@ -209,7 +208,7 @@ always @(posedge s00_axis_aclk) begin
 			axis_slave_done <= 1'b1;
 		end
 
-		if (axis_master_done) begin
+		if (axis_slave_done && in_fifo_empty) begin
 			axis_slave_done <= 1'b0;
 		end
 	end

hdl/aes_controller.v
@@ -54,7 +54,10 @@ reg [2:0]         state;
 wire              aes_done;
 wire              get_iv;
 
+reg controller_input_restart;
+
 reg [`KEY_S-1:0]  aes_key;
+reg [`WORD_S-1:0] __aes_cmd;
 
 reg [`IV_BITS-1:0]  iv;
 wire [`IV_BITS-1:0] iv_next;
@@ -107,8 +110,8 @@ assign aes_start_cipher = aes_start && aes_cipher_mode;
 assign aes_start_decipher = aes_start && aes_decipher_mode;
 assign aes_start_key_exp = aes_start && aes_key_exp_mode;
 
-assign aes128_mode = is_128bit_key(aes_cmd);
-assign aes256_mode = is_256bit_key(aes_cmd);
+assign aes128_mode = is_128bit_key(__aes_cmd);
+assign aes256_mode = is_256bit_key(__aes_cmd);
 
 assign need_iv =
                  cbc_flag ||
@@ -144,8 +147,8 @@ assign iv_next =
                  ofb_flag  ? ofb_iv :
                  {`IV_BITS{1'b0}};
 
-assign encrypt_flag = is_encryption(aes_cmd);
-assign decrypt_flag = is_decryption(aes_cmd);
+assign encrypt_flag = is_encryption(__aes_cmd);
+assign decrypt_flag = is_decryption(__aes_cmd);
 
 assign encryption_op = encrypt_flag
                        || ctr_flag
@@ -158,7 +161,7 @@ assign decryption_op = decrypt_flag
                        && !ofb_flag;
 generate
 if (ECB_SUPPORT) begin
-	assign ecb_flag = is_ECB_op(aes_cmd);
+	assign ecb_flag = is_ECB_op(__aes_cmd);
 
 	ecb ecb_mod(
 		.in_blk(in_blk),
@@ -174,7 +177,7 @@ endgenerate
 
 generate
 if (CBC_SUPPORT) begin
-	assign cbc_flag = is_CBC_op(aes_cmd);
+	assign cbc_flag = is_CBC_op(__aes_cmd);
 
 	cbc cbc_mod(
 		.encryption(encrypt_flag),
@@ -195,7 +198,7 @@ endgenerate
 
 generate
 if (CTR_SUPPORT) begin
-	assign ctr_flag = is_CTR_op(aes_cmd);
+	assign ctr_flag = is_CTR_op(__aes_cmd);
 
 	ctr ctr_mod(
 		.in_blk(in_blk),
@@ -213,7 +216,7 @@ endgenerate
 
 generate
 if (PCBC_SUPPORT) begin
-	assign pcbc_flag = is_PCBC_op(aes_cmd);
+	assign pcbc_flag = is_PCBC_op(__aes_cmd);
 
 	pcbc pcbc_mod(
 		.encryption(encrypt_flag),
@@ -234,7 +237,7 @@ endgenerate
 
 generate
 if (CFB_SUPPORT) begin
-	assign cfb_flag = is_CFB_op(aes_cmd);
+	assign cfb_flag = is_CFB_op(__aes_cmd);
 
 	cfb cfb_mod(
 		.encryption(encrypt_flag),
@@ -254,7 +257,7 @@ endgenerate
 
 generate
 if (OFB_SUPPORT) begin
-	assign ofb_flag = is_OFB_op(aes_cmd);
+	assign ofb_flag = is_OFB_op(__aes_cmd);
 
 	ofb ofb_mod(
 		.in_blk(in_blk),
@@ -302,7 +305,6 @@ always @(posedge clk) begin
 		aes_decipher_mode <= 1'b0;
 		aes_key_exp_mode <= 1'b0;
 		aes_cipher_mode <= 1'b1;
-		processing_done <= 1'b0;
 
 		in_fifo_read_tready <= 1'b0;
 		aes_key <= {`KEY_S{1'b0}};
@@ -316,6 +318,8 @@ always @(posedge clk) begin
 		case (state)
 			AES_GET_KEY_128:
 			begin
+				__aes_cmd <= aes_cmd;
+
 				in_fifo_read_tready <= 1'b1;
 
 				aes_decipher_mode <= 1'b0;
@@ -325,7 +329,6 @@ always @(posedge clk) begin
 				if (in_fifo_read_req) begin
 					state <= AES_START;
 
-					processing_done <= 1'b0;
 					in_fifo_read_tready <= 1'b0;
 
 					aes_key[`AES128_KEY_BITS-1 : 0] <= in_fifo_data;
@@ -368,7 +371,7 @@ always @(posedge clk) begin
 			begin
 				state <= AES_START;
 
-				if (!aes_op_in_progress && !aes_start)
+				if (!processing_done && !aes_op_in_progress && !aes_start)
 					in_fifo_read_tready <= 1'b1;
 
 				if (in_fifo_read_req) begin
@@ -381,14 +384,32 @@ always @(posedge clk) begin
 					aes_start <= 1'b1;
 				end
 
-				if (aes_done && axis_slave_done && in_fifo_empty) begin
-					processing_done <= 1'b1;
+				if (processing_done) begin
 					state <= AES_GET_KEY_128;
 				end
 			end
 			default:
 				state <= AES_GET_KEY_128;
 		endcase
+	end
+end
+
+always @(posedge clk) begin
+	if (reset == 1'b1) begin
+		controller_input_restart <= 1'b0;
+		processing_done <= 1'b0;
+	end else begin
+		if (axis_slave_done && in_fifo_empty)
+			controller_input_restart <= 1'b1;
+
+		if (aes_done && controller_input_restart) begin
+			processing_done <= 1'b1;
+		end
+
+		if (processing_done && out_fifo_empty) begin
+			controller_input_restart <= 1'b0;
+			processing_done <= 1'b0;
+		end
 	end
 end
 

hdl/zynq_aes_top.v
@@ -106,9 +106,6 @@ wire out_fifo_empty;
 wire out_fifo_write_tready;
 wire out_fifo_write_tvalid;
 
-// AXI master signals
-wire axis_master_done;
-
 // =====================================================================
 /*
 * AXI slave
@@ -128,7 +125,6 @@ aes_axi_stream_slave #(
 	.s00_axis_tdata(s00_axis_tdata),
 	.s00_axis_tstrb(s00_axis_tstrb),
 
-	.axis_master_done(axis_master_done),
 	.aes_controller_in_fifo_r_e(in_fifo_read_tready),
 
 	.axis_cmd(axis_cmd),
@@ -212,9 +208,7 @@ aes_axi_stream_master #(
 	.out_fifo_write_tready(out_fifo_write_tready),
 	.out_fifo_full(out_fifo_full),
 	.out_fifo_empty(out_fifo_empty),
-	.out_fifo_almost_full(out_fifo_almost_full),
-
-	.axis_master_done(axis_master_done)
+	.out_fifo_almost_full(out_fifo_almost_full)
 );
 
 endmodule

commit 50457b1e19809a31e47d08eef6a23deab307606b

fixup! hdl: aes_axi_stream_master: Fix tlast assertion
Fixes: ea0fc4c00d8f [hdl: Get rid of aes controller master --> slave feedback loop]
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

hdl/aes_axi_stream_master.v
@@ -65,10 +65,11 @@ wire [FIFO_DATA_WIDTH-1:0] axis_blk_shift;
 reg  [FIFO_ADDR_WIDTH-1:0] axis_word_cnt;
 reg  [FIFO_DATA_WIDTH-1:0] axis_blk;
 wire axis_transaction;
-wire axis_last_blk;
 wire axis_last_word;
 reg  axis_tvalid;
 wire axis_tlast;
+
+reg aes_last_blk;
 
 // FIFO logic
 
@@ -111,9 +112,8 @@ assign m00_axis_tstrb  = {(C_M_AXIS_TDATA_WIDTH/8){1'b1}};
 assign m00_axis_tvalid = axis_tvalid;
 assign m00_axis_tlast  = axis_tlast;
 
-assign axis_last_word = axis_last_blk && (axis_word_cnt == `Nb - 1'b1);
+assign axis_last_word = aes_last_blk && (axis_word_cnt == `Nb - 1'b1);
 assign axis_blk_shift = axis_blk >> axis_word_cnt * `WORD_S;
-assign axis_last_blk = processing_done && out_fifo_empty;
 assign axis_transaction = m00_axis_tready && axis_tvalid;
 assign axis_tlast =  axis_last_word;
 
@@ -145,6 +145,18 @@ always @(posedge m00_axis_aclk) begin
 				axis_word_cnt <= 1'b0;
 			end
 		end
+	end
+end
+
+always @(posedge m00_axis_aclk) begin
+	if (!m00_axis_aresetn) begin
+		aes_last_blk <= 1'b0;
+	end else begin
+		if (processing_done && out_fifo_empty)
+			aes_last_blk <= 1'b1;
+
+		if (axis_last_word && axis_transaction)
+			aes_last_blk <= 1'b0;
 	end
 end
 