33282c7cbdb736cde1ce84d70e2b8e6887006ef3
Ovidiu Panait <ovpanait@gmail.com>
2020-01-29 22:44:55 +0200

    hdl: axi_stream_slave: Implement axi stream slave as skid buffer
    Implement the axi stream slave as a skid buffer where all outputs are
    registered. This removes combinational loop from input to output, so it might
    provide better timing.
    
    Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

--- hdl/axi_stream_slave.v
+++ hdl/axi_stream_slave.v
@@ -1,5 +1,7 @@
 // Generic AXI4-STREAM slave interface
 
+// Generic AXI4-STREAM slave inteface implemented as a skid buffer.
+//
 // Received data is forwarded to a FIFO storage element. Synchronization with
 // the FIFO is achieved through fifo_busy/fifo_wren pair.
 
@@ -9,24 +11,127 @@ module axi_stream_slave #(
 )(
 	input                                      clk,
 	input                                      resetn,
-	output                                     tready,
+	output reg                                 tready,
 	input                                      tlast,
 	input                                      tvalid,
 	input [C_S_AXIS_TDATA_WIDTH - 1 : 0]       tdata,
 	input [(C_S_AXIS_TDATA_WIDTH / 8) - 1 : 0] tstrb,
 
 	input                                      fifo_busy,
-	output                                     fifo_wren,
-	output [C_S_AXIS_TDATA_WIDTH - 1 : 0]      fifo_data,
+	output reg                                 fifo_wren,
+	output reg [C_S_AXIS_TDATA_WIDTH - 1 : 0]  fifo_data,
 
-	output                                     stream_tlast
+	output reg                                 stream_tlast
 );
 
-assign tready = tvalid && !fifo_busy;
+localparam [1:0] IDLE = 2'b00;
+localparam [1:0] BUSY = 2'b01;
+localparam [1:0] FULL = 2'b10;
 
-assign fifo_wren = tvalid && tready;
-assign fifo_data = tdata;
-assign stream_tlast = tlast;
+reg [C_S_AXIS_TDATA_WIDTH - 1 : 0] fifo_data_skidbuf;
+reg stream_tlast_skidbuf;
+
+reg [1:0] state;
+
+wire insert, flow, fill, flush, remove;
+wire fifo_transaction, in_transaction;
+
+initial fifo_data_skidbuf = {C_S_AXIS_TDATA_WIDTH{1'b0}};
+initial fifo_data = {C_S_AXIS_TDATA_WIDTH{1'b0}};
+initial stream_tlast_skidbuf = 1'b0;
+initial stream_tlast = 1'b0;
+initial fifo_wren = 1'b0;
+initial tready = 1'b0;
+
+assign fifo_transaction = fifo_wren && !fifo_busy;
+assign in_transaction = tvalid && tready;
+
+assign insert = (state == IDLE) && in_transaction && !fifo_transaction;
+assign flow = (state == BUSY) && in_transaction && fifo_transaction;
+assign fill = (state == BUSY) && in_transaction && !fifo_transaction;
+assign flush = (state == FULL) && !in_transaction && fifo_transaction;
+assign remove = (state == BUSY) && !in_transaction && fifo_transaction;
+
+// Data path
+always @(posedge clk) begin
+	if (!resetn) begin
+		fifo_data_skidbuf <= {C_S_AXIS_TDATA_WIDTH{1'b0}};
+		fifo_data <= {C_S_AXIS_TDATA_WIDTH{1'b0}};
+
+		stream_tlast_skidbuf <= 1'b0;
+		stream_tlast <= 1'b0;
+	end else begin
+		if (insert || flow) begin
+			fifo_data <= tdata;
+			stream_tlast <= tlast;
+		end
+
+		if (flush) begin
+			fifo_data <= fifo_data_skidbuf;
+			stream_tlast <= stream_tlast_skidbuf;
+		end
+
+		if (fill) begin
+			fifo_data_skidbuf <= tdata;
+			stream_tlast_skidbuf <= tlast;
+		end
+	end
+end
+
+// Control path
+always @(posedge clk) begin
+	if (!resetn) begin
+		state <= IDLE;
+	end else begin
+		case (state)
+		IDLE:
+		begin
+			if (insert)
+				state <= BUSY;
+		end
+		BUSY:
+		begin
+			if (fill)
+				state <= FULL;
+
+			if (remove)
+				state <= IDLE;
+
+			if (flow)
+				state <= BUSY;
+		end
+		FULL:
+		begin
+			if (flush)
+				state <= BUSY;
+		end
+		endcase
+	end
+end
+
+always @(posedge clk) begin
+	if (!resetn) begin
+		fifo_wren <= 1'b0;
+	end else begin
+		if (insert)
+			fifo_wren <= 1'b1;
+
+		if (remove)
+			fifo_wren <= 1'b0;
+	end
+end
+
+always @(posedge clk) begin
+	if (!resetn) begin
+		tready <= 1'b1;
+	end else begin
+		if (flush)
+			tready <= 1'b1;
+
+		if (fill)
+			tready <= 1'b0;
+	end
+end
 
 `ifdef FORMAL
 
@@ -76,15 +181,6 @@ always @(posedge clk) begin
 			assert($past(tvalid));
 		end
 	end
-end
-
-always @(*) begin
-	assert(fifo_wren == (tvalid && tready));
-	assert(fifo_data == tdata);
-	assert(stream_tlast == tlast);
-
-	if (fifo_busy)
-		assert(!tready);
 end
 
 `endif
