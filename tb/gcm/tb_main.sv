ovpanait/zynq-aes

commit 359fc2634e35a67b8bffde4af18df9aa0e8c21af

hdl: gcm: Use standard en_{cipher, decipher} ports for aes module
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

tb/gcm/tb_main.sv
@@ -54,7 +54,8 @@ reg aes256_mode;
 wire                        aes_op_in_progress;
 wire [AES_BLK_BITS-1:0]     aes_alg_out_blk;
 wire [AES_BLK_BITS-1:0]     aes_alg_in_blk;
-wire                        aes_alg_start;
+wire                        aes_alg_en_cipher;
+wire                        aes_alg_en_decipher;
 wire                        aes_alg_done;
 reg  [AES_MAX_KEY_BITS-1:0] aes_alg_key;
 
@@ -83,10 +84,11 @@ gcm DUT (
 
 	.key_expanded(key_expanded),
 
+	.aes_alg_en_cipher(aes_alg_en_cipher),
+	.aes_alg_en_decipher(aes_alg_en_decipher),
 	.aes_alg_out_blk(aes_alg_out_blk),
 	.aes_alg_in_blk(aes_alg_in_blk),
 	.aes_alg_done(aes_alg_done),
-	.aes_alg_start(aes_alg_start),
 
 	.gcm_in_blk(gcm_in_blk),
 	.gcm_valid(gcm_valid),
@@ -191,11 +193,6 @@ end
    * AES algorithm control logic.
    * KEY needs to be expanded before any GCM operation.
  */
-always @(*) begin
-	aes_alg_en_cipher = aes_alg_start; // GCM only performs cipher operations (maybe TODO?)
-	aes_alg_en_decipher = 1'b0;
-end
-
 always @(posedge clk) begin
 	if (reset) begin
 		key_expanded <= 1'b0;

hdl/modes.v
@@ -648,8 +648,9 @@ module gcm #(
 
 	input [`BLK_S-1:0]              aes_alg_out_blk,
 	output reg [`BLK_S-1:0]         aes_alg_in_blk,
+	output reg                      aes_alg_en_cipher,
+	output reg                      aes_alg_en_decipher,
 	input                           aes_alg_done,
-	output reg                      aes_alg_start,
 
 	input [GCM_BLK_BITS-1:0]        gcm_in_blk,
 	input                           gcm_valid,
@@ -1006,7 +1007,8 @@ end
    * (ii) for computing the subkey H
  */
 always @(posedge clk) begin
-	aes_alg_start <= compute_subkey || gctr_aes_alg_start;
+	aes_alg_en_cipher <= compute_subkey || gctr_aes_alg_start;
+	aes_alg_en_decipher <= 1'b0;
 
 	if (compute_subkey)
 		aes_alg_in_blk <= {`BLK_S{1'b0}};

commit dc514ac097e3df47a8a2bc406d3f472b661245b4

tb: gcm: Remove duplicated aes_alg_en signals
Fixes: 359fc2634e35 ("hdl: gcm: Use standard en_{cipher, decipher} ports for aes module")
Signed-off-by: Ovidiu Panait <ovpanait@gmail.com>

tb/gcm/tb_main.sv
@@ -45,8 +45,6 @@ queue#(GCM_BLK_BITS) aes_keys_q;
 
 // AES algorithm signals
 
-reg aes_alg_en_cipher;
-reg aes_alg_en_decipher;
 reg aes_alg_en_key;
 
 reg aes128_mode;